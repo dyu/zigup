725ca6bbe6e460f0a5123f4e3604acee650c7699
dyu <david.yu.ftw@gmail.com>
2022-02-16 02:04:49 +0800

    use case/switch

--- release.sh
+++ release.sh
@@ -24,13 +24,17 @@ cross_compile_target(){
 archive_target(){
     NAME=${1%%.*}
     TARGET_DIR=zigup-$NAME
-    if [ "$TARGET_DIR" != *windows* ]; then
-        [ -e "$TARGET_DIR.tar.gz" ] && rm $TARGET_DIR.tar.gz
-        tar -cvzf $TARGET_DIR.tar.gz $TARGET_DIR
-    else
+    case "$NAME" in
+        *-windows-*)
         [ -e "$TARGET_DIR.zip" ] && rm $TARGET_DIR.zip
         zip -r $TARGET_DIR.zip $TARGET_DIR
-    fi
+        ;;
+        
+        *)
+        [ -e "$TARGET_DIR.tar.gz" ] && rm $TARGET_DIR.tar.gz
+        tar -cvzf $TARGET_DIR.tar.gz $TARGET_DIR
+        ;;
+    esac
 }
 
 for T in $TARGETS; do
@@ -54,15 +58,20 @@ REL_USER=dyu
 upload_target(){
     NAME=${1%%.*}
     TARGET_DIR=zigup-$NAME
-    FILE_SUFFIX='.zip'
-    [ "$TARGET_DIR" != *windows* ] && FILE_SUFFIX='.tar.gz'
-    echo Uploading $TARGET_DIR$FILE_SUFFIX
+    FILE_SUFFIX='.tar.gz'
+    case "$NAME" in
+        *-windows-*)
+        FILE_SUFFIX='.zip'
+        ;;
+    esac
+    UPLOAD_FILE=$TARGET_DIR$FILE_SUFFIX
+    echo Uploading $UPLOAD_FILE
     GITHUB_TOKEN=$REL_TOKEN GITHUB_AUTH_USER=$REL_USER github-release upload \
         --user $REL_USER \
         --repo zigup \
         --tag v$REL_VERSION \
-        --name $TARGET_DIR \
-        --file $TARGET_DIR$FILE_SUFFIX
+        --name $UPLOAD_FILE \
+        --file $UPLOAD_FILE
 }
 
 echo Tagging v$REL_VERSION
